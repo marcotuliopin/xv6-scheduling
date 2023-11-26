7c14a13deebdcfb1aa7cd3d81177459aa9389b62
marcotuliopin <marcotuliopin@gmail.com>
2023-11-26 15:48:25 -0300

    adiciona comentarios a trap.c

--- trap.c
+++ trap.c
@@ -13,7 +13,8 @@ struct gatedesc idt[256];
 extern uint vectors[];  // in vectors.S: array of 256 entry pointers
 struct spinlock tickslock;
 uint ticks;
-uint roundtimer;
+uint roundtimer; // Contador de ticks do processo atual. Necessario para
+                 // implementar o Round Robin.
 
 void
 tvinit(void)
@@ -52,9 +53,9 @@ trap(struct trapframe *tf)
     if(cpuid() == 0){
       acquire(&tickslock);
       ticks++;
-      roundtimer++;
-      procclock();
-      growprio();
+      roundtimer++; // Atualiza o contador do processo atual.
+      procclock(); // Atualiza contador de tempo dos processos.
+      growprio(); // Realiza politica de envelhecimento.
       wakeup(&ticks);
       release(&tickslock);
     }
@@ -104,7 +105,7 @@ trap(struct trapframe *tf)
   if(myproc() && myproc()->killed && (tf->cs&3) == DPL_USER)
     exit();
 
-  // Force process to give up CPU on clock tick.
+  // Force process to give up CPU when passed INTERV clock ticks.
   // If interrupts were on while locks held, would need to check nlock.
   if(myproc() && myproc()->state == RUNNING &&
      tf->trapno == T_IRQ0+IRQ_TIMER && roundtimer == INTERV)
